e7850a100a8ae9c87f186ad440117c2801ed15f4
raman <middha.raman@gmail.com>
2020-03-04 11:43:19 -0500

    added cosmos db initial code

---
+++ Samples/CosmosDBSample/CosmosDbClient/CosmosClientHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace CosmosDbClient
+{
+    public class CosmosClientHelper
+    {
+        readonly string DataBaseId = "Airline";
+        readonly string ContainerName = "AirlineData";
+
+        private Database database;
+
+        private Container container;
+        public async Task InitializeCosmosDB(CosmosClient client)
+        {
+            this.database = client.GetDatabase(DataBaseId);
+            //  client.ClientOptions.ConnectionMode = ConnectionMode.Gateway;
+
+            await InitializeContainer();
+        }
+
+        public async Task InitializeContainer()
+        {
+            //this.container =  database.GetContainer(ContainerName);
+
+            await Task.Run(() =>
+            {
+
+                //ContainerProperties containerProperties = new ContainerProperties()
+                //{
+                //    Id = Guid.NewGuid().ToString(),
+                //    PartitionKeyPath = "/pk",
+                //    IndexingPolicy = new IndexingPolicy()
+                //    {
+                //        Automatic = false,
+                //        IndexingMode = IndexingMode.Lazy,
+                //    }
+                //};
+                this.database.GetContainer(ContainerName);
+            });
+        }
+        public async Task RunItemsDemo()
+        {
+
+        }
+
+        public async Task Cleanup()
+        {
+
+        }
+
+
+    }
+}
--- Samples/CosmosDBSample/CosmosDbClient/Program.cs
+++ Samples/CosmosDBSample/CosmosDbClient/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
 
 namespace CosmosDbClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private CosmosClientHelper cosmosHelper;
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                
+                Program p = new Program();
+                await p.SetupCosmosDbAccount();
+                            
+            }
+          
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
         }
+
+        public async Task SetupCosmosDbAccount()
+        {
+            try
+            {
+                cosmosHelper = new CosmosClientHelper();
+
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                        .AddJsonFile("appSettings.json")
+                        .Build();
+
+                string endpoint = configuration["EndPointUrl"];
+                if (string.IsNullOrEmpty(endpoint))
+                {
+                    throw new ArgumentNullException("Please specify a valid endpoint in the appSettings.json");
+                }
+
+                string authKey = configuration["AuthorizationKey"];
+                if (string.IsNullOrEmpty(authKey) || string.Equals(authKey, "Super secret key"))
+                {
+                    throw new ArgumentException("Please specify a valid AuthorizationKey in the appSettings.json");
+                }
+
+                using (CosmosClient client = new CosmosClient(endpoint, authKey))
+                {
+                    await cosmosHelper.InitializeCosmosDB(client);
+                }
+            }
+            catch (CosmosException cre)
+            {
+                Console.WriteLine(cre.ToString());
+            }
+        }
+
+
     }
 }
